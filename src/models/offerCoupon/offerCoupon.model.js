92a22ef4a736a40b8e185acef6ee93ef50723c86
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-02-14 12:37:28 +0100

    refactor: update offer model

--- src/models/offerCoupon/offerCoupon.model.js
+++ src/models/offerCoupon/offerCoupon.model.js
@@ -8,8 +8,16 @@ const SpecialOfferSchema = new mongoose.Schema(
       ref: "Restaurants",
       required: true,
     },
+    imageUrl: { type: String },
+    couponType: {
+      type: String,
+      enum: {
+        values: ["percentage", "fixed price"],
+        message: "couponType must be either percentage or fixed price",
+      },
+    },
     active: { type: Boolean, default: true },
-    offer: { type: Number },
+    couponValue: { type: Number },
     startDate: { type: Date },
     endDate: { type: Date },
     expired: { type: Boolean, default: false },
