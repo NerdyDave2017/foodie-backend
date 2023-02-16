8fc912e93b2778c854ac0c75dfe715fdfe9e75bf
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-02-16 18:52:56 +0100

    feat: create dineIn schema model

---
+++ src/models/dineIn/dineIn.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const DineInSchema = new mongoose.Schema(
+  {
+    restaurantCost: { type: Number, required: true },
+    isDineActive: { type: Boolean, required: true },
+    openDineTime: { type: String, required: true },
+    closeDineTime: { type: String, required: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+    }, // references the _id field in the users collection for the user who added this dineIn
+    restaurantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurants",
+    }, // references the _id field in the users collection for the merchant who added this dineIn
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const DineIn = mongoose.model("DineIn", DineInSchema);
+
+module.exports = DineIn;
