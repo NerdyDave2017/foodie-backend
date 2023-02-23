c597a18b2f341bb722fa6ff089d8f3b22d6e4716
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-02-23 19:50:27 +0100

    rrefactor: update orders model

--- src/models/orders/orders.model.js
+++ src/models/orders/orders.model.js
@@ -5,14 +5,17 @@ const OrderSchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Users",
+      required: true,
     }, // references the _id field in the users collection (customer)
     restaurantId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Restaurants",
+      required: true,
     }, // references the _id field in the restaurants collection (merchant)
     driverId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Drivers",
+      default: null,
     }, // references the _id field in the drivers collection (driver)
     trackingId: { type: Number, required: true },
     items: [
@@ -24,34 +27,34 @@ const OrderSchema = new mongoose.Schema(
         quantity: { type: Number, require: true, default: 1 },
       },
     ],
-    orderType: { type: String, required: true }, // delivery or takeout
-    totalPrice: { type: Number, required: true },
-    status: { type: String, required: true }, // one of "pending", "accepted", "rejected", "in_progress", "delivered"
-    offerCoupon: { type: String },
-    discount: { type: Number },
-    tipValue: { type: Number },
-    adminCommission: { type: Number },
-    adminCommissionType: { type: String },
-    takeAway: { type: Boolean },
+    orderType: { type: String, required: true, default: "delivery" }, // delivery or takeout
+    totalPrice: { type: Number, required: true, default: null },
+    status: { type: String, required: true, default: "pending" }, // one of "pending", "accepted", "rejected", "in_progress", "delivered"
+    offerCoupon: { type: String, default: "" },
+    discount: { type: Number, default: null },
+    tipValue: { type: Number, default: null },
+    adminCommission: { type: Number, default: null },
+    adminCommissionType: { type: String, default: null },
+    takeAway: { type: Boolean, default: false },
     deliveryAddress: {
-      address: { type: String },
-      city: { type: String },
-      apartmentSuite: { type: String },
-      state: { type: String },
-      country: { type: String },
-      postalCode: { type: Number },
+      address: { type: String, default: "", required: true },
+      city: { type: String, default: "", required: true },
+      apartmentSuite: { type: String, default: "" },
+      state: { type: String, default: "", required: true },
+      country: { type: String, default: "", required: true },
+      postalCode: { type: Number, default: null },
       location: {
-        latitude: { type: Number },
-        longitude: { type: Number },
+        latitude: { type: Number, default: null },
+        longitude: { type: Number, default: null },
       },
-      line1: { type: Number },
-      line2: { type: Number },
-      email: { type: String },
+      line1: { type: Number, default: null },
+      line2: { type: Number, default: null },
+      email: { type: String, default: "" },
     },
-    deliveryCharge: { type: Number },
-    specialDiscount: { type: Array },
-    deliveryStartTime: { type: Date },
-    deliveryEndTime: { type: Date },
+    deliveryCharge: { type: Number, default: null },
+    specialDiscount: { type: Array, default: [] },
+    deliveryStartTime: { type: String, default: "" },
+    deliveryEndTime: { type: String, default: "" },
   },
   {
     timestamps: true,
