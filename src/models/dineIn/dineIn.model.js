0c4cc0cd7c46b68234ab29f5fd40f3cb19904114
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-02-23 12:39:17 +0100

    refactor: update dinin model

--- src/models/dineIn/dineIn.model.js
+++ src/models/dineIn/dineIn.model.js
@@ -2,18 +2,20 @@ const mongoose = require("mongoose");
 
 const DineInSchema = new mongoose.Schema(
   {
-    restaurantCost: { type: Number, required: true },
-    isDineActive: { type: Boolean, required: true },
-    openDineTime: { type: String },
-    closeDineTime: { type: String },
+    restaurantCost: { type: Number, required: true, default: null },
+    isDineActive: { type: Boolean, required: true, default: true },
+    openDineTime: { type: String, default: "" },
+    closeDineTime: { type: String, default: "" },
     isDineFulfiilled: { type: Boolean, required: true, default: false },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Users",
+      required: true,
     }, // references the _id field in the users collection for the user who added this dineIn
     restaurantId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Restaurants",
+      required: true,
     }, // references the _id field in the users collection for the merchant who added this dineIn
   },
   {
