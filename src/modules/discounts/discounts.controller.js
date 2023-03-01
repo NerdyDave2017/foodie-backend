aa2f94e978cdcb244e26c0e6ca823344625aa5ce
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-03-01 16:41:36 +0100

    refactor: updated discount deactivate function

--- src/modules/discounts/discounts.controller.js
+++ src/modules/discounts/discounts.controller.js
@@ -217,50 +217,23 @@ class SpecialDiscountController {
 
   deactivateDiscount = async (req, res, next) => {
     try {
-      const { id, restaurantId } = req.body;
-
-      const restaurantExist = await this.restaurantService.findRestaurantById(
-        restaurantId
-      );
-
-      if (!restaurantExist) {
-        throw next(new HttpException(404, "Restaurant not found"));
-      }
+      const { discountId } = req.params;
 
       const discountExist = await this.specialDiscountService.getDiscountById(
-        id
+        discountId
       );
 
       if (!discountExist) {
         throw next(new HttpException(404, "Discount not found"));
       }
 
-      const discount = await this.specialDiscountService.updateDiscountById(
-        id,
-        {
-          active: false,
-        }
-      );
-
-      const data = {
-        id: discount._id,
-        name: discount.name,
-        restaurantId: discount.restaurantId,
-        details: discount.details,
-        discountType: discount.discountType,
-        active: discount.active,
-        discountValue: discount.discountValue,
-        startDate: discount.startDate,
-        endDate: discount.endDate,
-        expired: discount.expired,
-        usabilityLimit: discount.usabilityLimit,
-        usageCount: discount.usageCount,
-      };
+      await this.specialDiscountService.updateDiscountById(discountId, {
+        active: false,
+      });
 
       return res.status(200).json({
         status: "success",
         message: "Discount deactivated",
-        data,
       });
     } catch (error) {
       next(error);
@@ -269,7 +242,7 @@ class SpecialDiscountController {
 
   deleteDiscount = async (req, res, next) => {
     try {
-      const discountId = req.params;
+      const { discountId } = req.params;
 
       const discountExist = await this.specialDiscountService.getDiscountById(
         discountId
